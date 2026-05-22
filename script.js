02b1cd3178c94f12afb152d1f84f6414aad84010
jasonlopez2705 <ifsmeb@proton.me>
2026-05-22 13:51:48 -0400

    svg-functionality

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const svgObject = document.getElementById("main__img");
+const paths = svgDoc.querySelectorAll("path");
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+document.addEventListener('mousemove', (e) => {
+    console.log(e);
+});
+
+// svgObject.addEventListener("load", () => {
+//     const svgDoc = svgObject.contentDocument;
+//     const paths = svgDoc.querySelectorAll("path");
+
+//     paths.forEach(path => {
+//         path.addEventListener("mouseenter", () => {
+//             path.style.stroke = "gold";
+//         });
+
+//         path.addEventListener("mouseleave", () => {
+//             path.style.stroke = "white";
+//         });
+//     });
+// });
